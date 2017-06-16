92d679490d54cf748fca6468dbbc704e374be2ee
Steve <l7shao@uwaterlo.ca>
2017-06-16 14:59:54 -0400

    User Model Set up!

--- routers/users.js
+++ routers/users.js
@@ -3,10 +3,28 @@
  */
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../modles/user');
 
 //Register
-router.get('/register',function(req,res){
-  res.send('Register');
+router.post('/register',function(req,res){
+
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  User.addUser(newUser,function(err,user){
+    if(err){
+      res.json({success:false, msg:'Failed to register user'});
+    }else{
+      res.json({success:true, msg:'Succeed to register user'});
+    }
+  });
 });
 //Authentication
 router.get('/authentication',function(req,res){
@@ -15,10 +33,6 @@ router.get('/authentication',function(req,res){
 //profile
 router.get('/profile',function(req,res){
   res.send('Profile');
-});
-//Validate
-router.get('/validate',function(req,res){
-  res.send('Validate');
 });
 
 module.exports = router;
