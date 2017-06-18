c6d6a2872fe380beecc0db0a356d8b14e6fc1a81
Steve <l7shao@uwaterlo.ca>
2017-06-18 19:22:41 -0400

    Auth module added

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {ValidateService} from '../../services/validate.service'
 import {FlashMessagesService} from 'angular2-flash-messages';
+import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router'
+
 
 @Component({
   selector: 'app-register',
@@ -13,7 +16,11 @@ export class RegisterComponent implements OnInit {
   email:String;
   password:String;
 
-  constructor(private ValidateService:ValidateService,private flashmessage:FlashMessagesService) { }
+  constructor(private router: Router,
+    private authService: AuthService,
+    private ValidateService:ValidateService,
+    private flashmessage:FlashMessagesService
+  ) { }
 
   ngOnInit() {
   }
@@ -34,5 +41,16 @@ export class RegisterComponent implements OnInit {
       this.flashmessage.show('Please fill correct email',{cssClass:'alert-danger',timeout:3000})
       return false;
     }
+    //register user
+    this.authService.registerUser(user).subscribe(data=>{
+      if(data.success){
+        this.flashmessage.show('You just registered successfully',{cssClass:'alert-success',timeout:3000})
+        this.router.navigate(['/login']);
+      }else{
+        this.flashmessage.show('Something went wrong',{cssClass:'alert-danger',timeout:3000})
+        this.router.navigate(['/register']);
+      }
+    });
+
   }
 }
